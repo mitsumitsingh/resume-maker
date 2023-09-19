c9f869f248df80c32e943d1c93e12cf750ded46e
Sumit Kumar Singh <sksingq@gmail.com>
2023-09-19 16:54:59 +0800

    added migration script

--- src/UserGroupMigration.java
+++ src/UserGroupMigration.java
@@ -1,154 +1,78 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-class User {
-    String name;
-    Map<String, Map<String, String>> attributes = new HashMap<>();
-    List<String> groups = new ArrayList<>();
+public class TibcoUserGroupExtractor {
 
-    User(String name) {
-        this.name = name;
-    }
-}
+    private static final Pattern USER_PATTERN = Pattern.compile("(?<=\bU\+)[\w@]+(?=\b)");
+    private static final Pattern GROUP_PATTERN = Pattern.compile("(?<=\bG\+)[\w]+(?=\b)");
+    private static final Pattern GROUP_MEMBERSHIP_PATTERN = Pattern.compile("(?<=\bM\+)[\w@]+\s+[\w]+(?=\b)");
 
-class Group {
-    String name;
-    Map<String, Map<String, String>> attributes = new HashMap<>();
+    public static void main(String[] args) throws IOException {
+        // Get the exported file name from the command line arguments.
+        String exportedFileName = args[0];
 
-    Group(String name) {
-        this.name = name;
-    }
-}
+        // Create a map to store the extracted user, group, and membership information.
+        Map<String, Map<String, List<String>>> extractedData = new HashMap<>();
 
-public class UserGroupMigration {
-    private static Map<String, User> users = new HashMap<>();
-    private static Map<String, Group> groups = new HashMap<>();
-    private static User currentUser = null;
-
-    public static void main(String[] args) {
-        String controlFilePath = "control_file.txt"; // Provide the correct file path
-
-        try (BufferedReader br = new BufferedReader(new FileReader(controlFilePath))) {
+        // Create a buffered reader to read the exported file.
+        try (BufferedReader reader = new BufferedReader(new FileReader(exportedFileName))) {
             String line;
-            while ((line = br.readLine()) != null) {
-                line = line.trim();
-                if (line.isEmpty() || line.startsWith(";")) {
-                    continue; // Skip comments and empty lines
+            while ((line = reader.readLine()) != null) {
+                // Ignore comment lines and blank lines.
+                if (line.startsWith("#") || line.isBlank()) {
+                    continue;
                 }
 
-                String[] parts = line.split(",");
-                char command = parts[0].charAt(0);
-                String[] commandArgs = new String[parts.length - 1];
-                System.arraycopy(parts, 1, commandArgs, 0, commandArgs.length);
-
-                processInstruction(command, commandArgs);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Now you have the users and groups data in the "users" and "groups" maps.
-        // You can process this data further or perform any necessary actions.
-    }
-
-    private static void processInstruction(char command, String[] args) {
-        switch (command) {
-            case 'R':
-                resetData();
-                break;
-            case 'U':
-                if (args[0].charAt(0) == '+') {
-                    addUser(args[0].substring(1));
-                } else if (args[0].charAt(0) == '-') {
-                    deleteUser(args[0].substring(1));
+                // Extract the user information.
+                Matcher userMatcher = USER_PATTERN.matcher(line);
+                if (userMatcher.matches()) {
+                    String username = userMatcher.group();
+                    extractedData.computeIfAbsent(username, k -> new HashMap<>()).put("attributes", List.of(line.split("\\s+")[1]));
+                    continue;
                 }
-                break;
-            case 'G':
-                if (args[0].charAt(0) == '+') {
-                    addGroup(args[0].substring(1));
-                } else if (args[0].charAt(0) == '-') {
-                    deleteGroup(args[0].substring(1));
+
+                // Extract the group information.
+                Matcher groupMatcher = GROUP_PATTERN.matcher(line);
+                if (groupMatcher.matches()) {
+                    String groupName = groupMatcher.group();
+                    extractedData.computeIfAbsent(groupName, k -> new HashMap<>()).put("attributes", List.of(line.split("\\s+")[1]));
+                    continue;
                 }
-                break;
-            case 'A':
-                addAttribute(args);
-                break;
-            case 'V':
-                changeAttributeValue(args);
-                break;
-            case 'M':
-                boolean add = args[0].charAt(0) == '+';
-                changeMembership(args[0].substring(1), args[1], add);
-                break;
-        }
-    }
 
-    private static void resetData() {
-        users.clear();
-        groups.clear();
-        currentUser = null;
-    }
+                // Extract the group membership information.
+                Matcher groupMembershipMatcher = GROUP_MEMBERSHIP_PATTERN.matcher(line);
+                if (groupMembershipMatcher.matches()) {
+                    String username = groupMembershipMatcher.group(1);
+                    String groupName = groupMembershipMatcher.group(2);
 
-    private static void addUser(String name) {
-        currentUser = new User(name);
-        users.put(name, currentUser);
-    }
-
-    private static void deleteUser(String name) {
-        users.remove(name);
-        currentUser = null;
-    }
-
-    private static void addGroup(String name) {
-        groups.put(name, new Group(name));
-    }
-
-    private static void deleteGroup(String name) {
-        groups.remove(name);
-    }
-
-    private static void addAttribute(String[] args) {
-        if (currentUser != null) {
-            String attribute = args[0];
-            String type = args[1];
-            String length = args[2];
-            String changer = args[3];
-            String defaultValue = args.length > 4 ? args[4] : "";
-
-            Map<String, String> attributeInfo = new HashMap<>();
-            attributeInfo.put("type", type);
-            attributeInfo.put("length", length);
-            attributeInfo.put("changer", changer);
-            attributeInfo.put("default", defaultValue);
-
-            currentUser.attributes.put(attribute, attributeInfo);
-        }
-    }
-
-    private static void changeAttributeValue(String[] args) {
-        if (currentUser != null) {
-            String attribute = args[0];
-            String value = args.length > 1 ? args[1] : "";
-            Map<String, String> attributeInfo = currentUser.attributes.get(attribute);
-
-            if (attributeInfo != null) {
-                attributeInfo.put("value", value);
+                    extractedData.computeIfAbsent(username, k -> new HashMap<>()).computeIfAbsent("groups", k -> new HashMap<>()).put(groupName, List.of());
+                    continue;
+                }
             }
         }
-    }
 
-    private static void changeMembership(String userName, String groupName, boolean add) {
-        if (users.containsKey(userName) && groups.containsKey(groupName)) {
-            User user = users.get(userName);
-            if (add) {
-                user.groups.add(groupName);
-            } else {
-                user.groups.remove(groupName);
+        // Print the extracted data.
+        for (Map.Entry<String, Map<String, List<String>>> entry : extractedData.entrySet()) {
+            String username = entry.getKey();
+            Map<String, List<String>> userData = entry.getValue();
+
+            System.out.println("User:");
+            System.out.println("  Username: " + username);
+            System.out.println("  Attributes:");
+            for (String attribute : userData.get("attributes")) {
+                System.out.println("    " + attribute);
+            }
+
+            System.out.println("  Groups:");
+            for (Map.Entry<String, List<String>> groupEntry : userData.get("groups").entrySet()) {
+                String groupName = groupEntry.getKey();
+                System.out.println("    " + groupName);
             }
         }
     }
